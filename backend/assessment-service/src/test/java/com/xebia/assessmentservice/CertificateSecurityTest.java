2fc397c2344b0e3a61cac384788be173fc0b926b
Abhijeet0Tiwari <rahul49shukla@gmail.com>
2026-07-11 22:44:30 +0530

    Convert CertificateSecurityTest to a pure JUnit5/Mockito/ReflectionTestUtils unit test

--- backend/assessment-service/src/test/java/com/xebia/assessmentservice/CertificateSecurityTest.java
+++ backend/assessment-service/src/test/java/com/xebia/assessmentservice/CertificateSecurityTest.java
@@ -4,26 +4,22 @@ import com.xebia.assessmentservice.controller.CertificateController;
 import com.xebia.assessmentservice.service.CertificateService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
 public class CertificateSecurityTest {
-
-    @MockBean
-    private CertificateService certificateService;
-
-    @Autowired
-    private CertificateController certificateController;
 
     @Test
     public void testUnauthorizedAccessToCertificateBlocked() {
+        CertificateService certificateService = Mockito.mock(CertificateService.class);
+        CertificateController certificateController = new CertificateController();
+        
+        ReflectionTestUtils.setField(certificateController, "certificateService", certificateService);
+
         String arbitraryUuid = "arbitrary-invalid-uuid";
         Mockito.when(certificateService.getCertificateDetails(arbitraryUuid)).thenReturn(null);
 
